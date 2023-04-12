12eadd3245cde53d7505d21a4c5cbd7b567f00b2
Alessandra Wong <awong@pdx.edu>
2023-04-12 14:24:21 -0700

    Added pages for previous work, projects
    Added "Previous Work" and "Projects" pages to the website with
    their associated views and URLs and
    added links in the navbar.

--- website/urls.py
+++ website/urls.py
@@ -4,5 +4,7 @@ from website import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
-    path("contact", views.contact, name="contact"),
+    path("contact/", views.contact, name="contact"),
+    path("previous-work/", views.previous_work, name="previous-work"),
+    path("projects/", views.projects, name="projects"),
 ]
--- website/views.py
+++ website/views.py
@@ -1,20 +1,22 @@
 from django.shortcuts import HttpResponse, render
 
+
 # Create your views here.
 def home(request):
-    return render(
-        request,
-        'website/homepage.html'
-    )
+    return render(request, "website/homepage.html")
+
 
 def about(request):
-    return render(
-        request,
-        "website/about.html"
-    )
+    return render(request, "website/about.html")
+
 
 def contact(request):
-    return render(
-        request,
-        "website/contact.html"
-    )
+    return render(request, "website/contact.html")
+
+
+def previous_work(request):
+    return render(request, "website/previous-work.html")
+
+
+def projects(request):
+    return render(request, "website/projects.html")
